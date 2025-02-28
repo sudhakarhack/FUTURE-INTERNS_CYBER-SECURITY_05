b2f9cce6331e4130e0d08d908e9e244474a0a53f
SUDHAKAR ILLA <142078109+sudhakarhack@users.noreply.github.com>
2025-02-28 23:52:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# FUTURE-INTERNS_CYBER-SECURITY_05
-Email Analysis
+Email Meta Analysis
+=====
+Ever wonder what was in your email?
+
+I did, so I wrote some python scripts to analyze it! 
+
+You can check out the analysis of my own email [on my website](http://austingwalters.com/analyzing-email-data/)
+
+## Usage
+
+* Must place python scripts in the same folder as mbox
+* Rename .mbox as 'mail.mbox'
+* Need folder wordFrequency (should be present)
+* Add name or any other words to skip to stdWords.csv
+* Run <filename>.py as desired
+* Wait ~2 minutes per Gb
+* Load .csv into excel or sheets
+
+## Required Tuning
+
+* Everyone has different *spam*, either edit scripts or remove on .csv
+* Edit stdWords.csv to skip words when analyzing frequency
+* To make graphs I used Google Sheets, I removed common words
+* I recommend adding companies to stdWords.csv, especially if you receive updates
+
+## Minor Disclaimer
+
+I wrote this *extremely* rough collection of scripts which work well for me with minor editing.
+
+I make no claims they will work for you, and if you make any upgrades please merge them in! :)
---
+++ parseWords.py
@@ -0,0 +1,168 @@
+import mailbox
+import csv
+from parseDates import parseDate
+import re
+import string
+
+def getcharsets(msg):
+    charsets = set({})
+    for c in msg.get_charsets():
+        if c is not None:
+            charsets.update([c])
+    return charsets
+
+def handleerror(errmsg, emailmsg,cs):
+    print(errmsg)
+    print("This error occurred while decoding with ",cs," charset.")
+    print("These charsets were found in the one email.",getcharsets(emailmsg))
+    print("This is the subject:",emailmsg['subject'])
+    print("This is the sender:",emailmsg['From'])
+
+def getbodyfromemail(msg):
+    body = None
+    #Walk through the parts of the email to find the text body.    
+    if msg.is_multipart():    
+        for part in msg.walk():
+
+            # If part is multipart, walk through the subparts.            
+            if part.is_multipart(): 
+
+                for subpart in part.walk():
+                    if subpart.get_content_type() == 'text/plain':
+                        # Get the subpart payload (i.e the message body)
+                        body = subpart.get_payload(decode=True) 
+                        #charset = subpart.get_charset()
+
+            # Part isn't multipart so get the email body
+            elif part.get_content_type() == 'text/plain':
+                body = part.get_payload(decode=True)
+                #charset = part.get_charset()
+
+    # If this isn't a multi-part message then get the payload (i.e the message body)
+    elif msg.get_content_type() == 'text/plain':
+        body = msg.get_payload(decode=True) 
+
+   # No checking done to match the charset with the correct part. 
+    for charset in getcharsets(msg):
+        try:
+            body = body.decode(charset)
+        except LookupError:
+            handleerror("LookupError: encountered.", msg,charset)
+        except UnicodeEncodeError:
+            handleerror("UnicodeEncodeError: encountered.",msg,charset)
+        except UnicodeDecodeError:
+            handleerror("UnicodeDecodeError: encountered.",msg,charset)
+        except AttributeError:
+             handleerror("AttributeError: encountered" ,msg,charset)
+    return body    
+
+stdWords = []
+reader = csv.reader(open("stdWords.csv", "rb"))
+for row in reader:
+   stdWords.append(row[0]) 
+
+#stdWords = set(stdWords)
+
+''' ___MAIN___ '''
+
+writer = csv.writer(open("wordFrequency.csv", "wb"))
+
+words = dict()
+wordsYear = dict()
+wordsMonth = dict()
+
+emailCount = 0
+
+for message in mailbox.mbox('mail.mbox'):
+
+    day, month, year, date = parseDate(message['date'])
+
+    # body contains lowercase only, no numbers either
+    body = getbodyfromemail(message)
+    if body is None:
+        body = "None"
+
+    try:
+        body = body.encode('ascii', 'ignore')
+        body = body.lower()
+
+        body = body.replace('@', 'zzz')
+
+        # Removes any punctuation                                                               
+        table = string.maketrans("","")
+        body = body.translate(table, string.punctuation)
+
+        
+        # Split by any puncuiation, i.e. \r, \n, \t, ' '
+        body = re.findall(r"[\w']+", body)
+
+    except UnicodeEncodeError:
+        print "UnicodeEncodeError!"
+        body = "None"
+    except UnicodeDecodeError:
+        print "UnicodeDecodeError!"
+        body = "None"
+    except AttributeError:
+        print "AttributeError!"
+        body = "None"
+
+    print 'count: ', emailCount
+    emailCount += 1
+    
+    day, month, year, date = parseDate(message['date'])
+
+
+    # Count for each word
+    for word in body:
+        
+        if word is None or word.find('0x') is not -1:
+            word = None
+        if word is None or word.find('zzz') is not -1:
+            word = None
+
+        # Add words to word
+        if word is not None and word.find("http") is -1:
+            word = re.sub(r'\d', '', word)
+            if len(word) < 21 and len(word) > 3:
+                if word not in words:
+                    words[word] = 1
+                else:
+                    words[word] += 1
+
+                # Month associated with words
+                if month not in wordsMonth:
+                    wordsMonth[month] = dict()
+                if word not in wordsMonth[month]:
+                    wordsMonth[month][word] = 1
+                else:
+                    wordsMonth[month][word] += 1
+
+                # Year associated with words
+                if year not in wordsYear:
+                    wordsYear[year] = dict()
+                if word not in wordsYear[year]:
+                    wordsYear[year][word] = 1
+                else:
+                    wordsYear[year][word] += 1
+
+
+writer.writerow(["Word", "Count"])
+for word in words:
+    if word not in stdWords:
+        writer.writerow([word.encode('utf8'), words[word]])
+
+# Writes the word frequency for every month
+for month in wordsMonth:
+    monthWriter = csv.writer(open("wordFrequency/" + month + "_wordFrequency.csv", "wb"))
+    monthWriter.writerow(["Word","Count"])
+    for word in wordsMonth[month]:
+        if word not in stdWords:
+            monthWriter.writerow([word.encode('utf8'), wordsMonth[month][word]])
+
+# Writes the word frequency for every year
+for year in wordsYear:
+    yearWriter = csv.writer(open("wordFrequency/" + year + "_wordFrequency.csv", "wb"))
+    yearWriter.writerow(["Word","Count"])
+    for word in wordsYear[year]:
+        if word not in stdWords:
+            yearWriter.writerow([word.encode('utf8'), wordsYear[year][word]])
